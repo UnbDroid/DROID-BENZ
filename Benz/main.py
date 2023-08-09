1ab0aab3b0bc383eaf37d6ba4f29d2faa10c200d
luisf <luisfelipehcarmona@gmail.com>
2023-08-09 16:10:09 -0300

    CONVERSA ENTRE BRICKS

--- Benz/main.py
+++ Benz/main.py
@@ -5,12 +5,39 @@ from pybricks.parameters import Stop, Direction, Button
 from pybricks.tools import wait, StopWatch, DataLog
 #from pybricks.media.ev3dev import SoundFile, ImageFile
 
-from modules.motors import *
-from modules.colors import *
-from modules.detect import *
-from modules.places import *
-from modules.claw import *
-import time
+#!/usr/bin/env pybricks-micropython
+
+# Before running this program, make sure the client and server EV3 bricks are
+# paired using Bluetooth, but do NOT connect them. The program will take care
+# of establishing the connection.
+
+# The server must be started before the client!
+
+from pybricks.messaging import BluetoothMailboxClient, TextMailbox
+
+# This is the name of the remote EV3 or PC we are connecting to.
+SERVER = 'ev3dev'
+
+client = BluetoothMailboxClient()
+mbox = TextMailbox('greeting', client)
+
+print('establishing connection...')
+client.connect(SERVER)
+print('connected!')
+
+# In this program, the client sends the first message and then waits for the
+# server to reply.
+mbox.send('hello!')
+mbox.wait()
+print(mbox.read())
+
+
+# from modules.motors import *
+# from modules.colors import *
+# from modules.detect import *
+# from modules.places import *
+# from modules.claw import *
+# import time
 
 #Declaração de variáveis globais e objetos
 
@@ -24,7 +51,7 @@ import time
 
 #    circle_right()
 #recognize_fisrt()
-reposition("Blue")
+# reposition("Blue")
 #turn_90_left()
 #move_forward_cm(60)
 #i = 0
