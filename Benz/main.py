cd0882cee7b30ea2ad80f17415fafbfb76f80526
luisf <luisfelipehcarmona@gmail.com>
2023-08-15 17:06:40 -0300

    reconhecimento de cores e altura do tubo do segundo brick

--- Benz/main.py
+++ Benz/main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env pybricks-micropython
+from pybricks.nxtdevices import ColorSensor as ColorNxt
+from pybricks.ev3devices import ColorSensor
+from pybricks.parameters import Port
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (InfraredSensor, UltrasonicSensor)
 from pybricks.parameters import Stop, Direction, Button
 from pybricks.tools import wait, StopWatch, DataLog
+import time
 #from pybricks.media.ev3dev import SoundFile, ImageFile
+
 
 #!/usr/bin/env pybricks-micropython
 
@@ -15,20 +20,30 @@ from pybricks.tools import wait, StopWatch, DataLog
 # The server must be started before the client!
 
 from pybricks.messaging import BluetoothMailboxClient, TextMailbox
-
+time.sleep(2)
 # This is the name of the remote EV3 or PC we are connecting to.
 SERVER = 'ev3dev'
-
+print("tentando ;)")
 client = BluetoothMailboxClient()
 mbox = TextMailbox('greeting', client)
 
 print('establishing connection...')
 client.connect(SERVER)
 print('connected!')
+mbox.send("Hello")
+
 
 # In this program, the client sends the first message and then waits for the
 # server to reply.
+from modules.colors import *
 while True:
-  mbox.wait()
+  print(mbox.read())
   if mbox.read() == "True":
-    mbox.send(check())
+    print("checkando")
+    txt = check()
+    mbox.send(txt)
+
+#print(see())
+# while True:
+#   print(check())
+#   wait(1000)
--- Benz/modules/colors.py
+++ Benz/modules/colors.py
@@ -1,9 +1,10 @@
 from pybricks.parameters import Color
 from pybricks.parameters import Port
 from pybricks.ev3devices import ColorSensor
+from pybricks.nxtdevices import ColorSensor as ColorNxt
 
-sensor_color = ColorSensor(Port.S1)
-sensor_sup = ColorSensor(Port.S2)
+sensor_color = ColorSensor(Port.S2)
+sensor_sup = ColorNxt(Port.S3)
 
 
 def calibration(sensor):
@@ -14,75 +15,72 @@ def calibration(sensor):
 
 
 # arrumar o marrom e verde
-red = [[30, 5, 0], [49, 25, 20]]  # check
+red = [[50, 3, 0], [72, 25, 10]]  # check
 
-blue = [[0, 15, 43], [17, 34, 65]]  # check
+blue = [[0, 7, 10], [12, 19, 21]]  # check
 
-yellow = [[35, 64, 11], [55, 82, 30]]  # check
+yellow = [[35, 64, 11], [55, 82, 30]]  
 
-green = [[30, 5, 0], [49, 25, 20]]
-
-black = [[2, 5, 0], [15, 15, 10]]  # check
-
-brown = [[20, 10, 0], [35, 30, 18]]  # check
+green = [[0, 18, 0], [12, 32, 12]]#check
 
 
-# 
+brown = [[62, 62, 57], [78, 80, 70]]  
 
-def red():
+
+def calibration(sensor):
+    print(sensor.rgb())
+
+
+def Red():
     rgb = sensor_color.rgb()
     return ((red[0][0] <= rgb[0] and rgb[0] <= red[1][0]) and (red[0][1] <= rgb[1] and rgb[1] <= red[1][1]) and (red[0][2] <= rgb[2] and rgb[2] <= red[1][2]))
 
 
-def black():
+def Green():
+    rgb = sensor_color.rgb()
+    return ((green[0][0] <= rgb[0] and rgb[0] <= green[1][0]) and (green[0][1] <= rgb[1] and rgb[1] <= green[1][1]) and (green[0][2] <= rgb[2] and rgb[2] <= green[1][2]))
+
+
+def Black():
     rgb = sensor_color.rgb()
     return ((black[0][0] <= rgb[0] and rgb[0] <= black[1][0]) and (black[0][1] <= rgb[1] and rgb[1] <= black[1][1]) and (black[0][2] <= rgb[2] and rgb[2] <= black[1][2]))
 
 
-def blue():
+def Blue():
     rgb = sensor_color.rgb()
     return ((blue[0][0] <= rgb[0] and rgb[0] <= blue[1][0]) and (blue[0][1] <= rgb[1] and rgb[1] <= blue[1][1]) and (blue[0][2] <= rgb[2] and rgb[2] <= blue[1][2]))
 
 
-def yellow():
+def Yellow():
     rgb = sensor_color.rgb()
     return ((yellow[0][0] <= rgb[0] and rgb[0] <= yellow[1][0]) and (yellow[0][1] <= rgb[1] and rgb[1] <= yellow[1][1]) and (yellow[0][2] <= rgb[2] and rgb[2] <= yellow[1][2]))
 
 
-def brown():
-    rgb = sensor_color_right.rgb()
+def Brown():
+    rgb = sensor_color.rgb()
     return ((brown[0][0] <= rgb[0] and rgb[0] <= brown[1][0]) and (brown[0][1] <= rgb[1] and rgb[1] <= brown[1][1]) and (brown[0][2] <= rgb[2] and rgb[2] <= brown[1][2]))
 
 
 def see():
-    if red():
-        print("vi vermelho")
+    if Red():
         return "Red"
-    elif blue():
-        print("vi azul")
+    elif Blue():
         return "Blue"
-    elif black():
-        print("vi preto")
-        return "Black"
-    # elif green():
-    #     return "Green"
-    elif brown():
-        print("vi marrom")
+    elif Green():
+        return "Green"
+    elif Brown():
         return "Brown"
-    elif yellow():
-        print("vi amarelo")
+    elif Yellow():
         return "Yellow"
     else:
-        print("vi branco")
         return "White"
 
 
 def size():
-    if sensor_sup().reflection() <= 50:
+    if sensor_sup.reflection() <= 20:
         return "10"
     else:
         return "15"
-
 
 
 def check():
