f3dbc4491b6e5b3b412fe83da83f8e1ad4c94125
luisf <luisfelipehcarmona@gmail.com>
2023-09-14 16:29:36 -0300

    ajuste

--- Benz/main.py
+++ Benz/main.py
@@ -9,21 +9,37 @@ from pybricks.tools import wait, StopWatch, DataLog
 import time
 from modules.colors import *
 #from pybricks.media.ev3dev import SoundFile, ImageFile
-
+from pybricks.messaging import BluetoothMailboxClient, TextMailbox
 
 #!/usr/bin/env pybricks-micropython
 
+#####
+SERVER = 'ev3dev'
 
+client = BluetoothMailboxClient()
+mbox = TextMailbox('greeting', client)
+
+print('establishing connection...')
+client.connect(SERVER)
+print('connected!')
+
+# In this program, the client sends the first message and then waits for the
+# server to reply.
+mbox.send('hello!')
+mbox.wait()
+print(mbox.read())
+
+####
 # Before running this program, make sure the client and server EV3 bricks are
 # paired using Bluetooth, but do NOT connect them. The program will take care
 # of establishing the connection.
 
 # The server must be started before the client!
 
-from pybricks.messaging import BluetoothMailboxClient, TextMailbox
+
 time.sleep(2)
 # This is the name of the remote EV3 or PC we are connecting to.
-while True:
+"""while True:
   server = BluetoothMailboxServer()
   mbox = TextMailbox('greeting', server)
   # The server must be started before the client!
@@ -44,11 +60,19 @@ while True:
       mbox.send(txt)
     elif mbox.read() == "End":
       mbox.close()
-      connected = False
+      connected = False"""
+
+
+
 
 # In this program, the client sends the first message and then waits for the
 # server to reply.
-
+while connected:
+    print(mbox.read())
+    if mbox.read() == "True":
+      print("checkando")
+      txt = check()
+      mbox.send(txt)
 
 
 
