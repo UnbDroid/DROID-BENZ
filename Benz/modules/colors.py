7d1010fc32b6ebcb8f4175d562aefcdc3f471e32
luisf <luisfelipehcarmona@gmail.com>
2023-10-10 14:33:10 -0300

    reajuste entrada

--- Benz/modules/colors.py
+++ Benz/modules/colors.py
@@ -69,16 +69,16 @@ yellow_left = [[50, 71, 4], [66, 87, 20]]
 yellow_right = [[37, 60, 8], [53, 76, 24]]#ok
 
 yell_i_black_left = [[63, 77, 11], [79, 93, 27]]
-yell_i_black_right = [[63, 77, 11], [79, 93, 27]]
-
-green_left = [[14,35,0],[24,55,20]]
-green_right = [[5,32,0],[20,45,20]] #ok
+yell_i_black_right = [[63, 77, 11], [79, 93, 27]] #ok
 
 black_left = [[0,0, 0],[20, 21, 18]]
 black_right = [[0,0,0],[7, 24, 21]] #ok
 
-brown_left = [[20,10,0],[35,30,18]]
-brown_right = [[24,9,0],[45,32,15]] 
+green_left = [[2, 25, 5], [18, 41, 21]]
+green_right = [[0, 22, 7], [14, 38, 23]]
+
+brown_left = [[14, 8, 2], [30, 24, 18]]
+brown_right = [[7, 6, 6], [23, 22, 22]]
 
 white_left = [[63, 81, 63], [79, 97, 79]]
 white_right = [[45, 71, 92], [61, 87, 108]]
@@ -157,6 +157,17 @@ def brownRight() :
     return ( (brown_right[0][0] <= rgb[0] and rgb[0] <=brown_right[1][0]) and (brown_right[0][1] <= rgb[1] and rgb[1] <=brown_right[1][1]) and (brown_right[0][2] <= rgb[2] and rgb[2] <=brown_right[1][2]) )
 
 
+def and_saw_red():
+    return redRight() and redLeft()
+
+
+def and_saw_green():
+    return greenRight() and greenLeft()
+
+
+def and_saw_brown():
+    return brownRight() and brownLeft()
+
 #Declaration of detected colors
 
 def saw_wall():
@@ -179,7 +190,10 @@ def saw_blue_white():
 
 #def saw_brown():
  #   return brownRight() or brownLeft()
-    
+
+def inside():
+    return and_saw_blue() or and_saw_brown() or and_saw_green() or and_saw_red()
+
 def saw_white():
     return whiteRight() or whiteLeft()
 def saw_yellow_black():
